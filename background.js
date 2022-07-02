5e50e38978407230f000ed9906f4fa2feca7623e
Przemyslaw Pajak <przemyslaw.pajak@creative-works.us>
2022-07-02 14:34:28 +0200

    Added auto click

--- background.js
+++ background.js
@@ -1,6 +1,12 @@
-let color = '#3aa757';
-
 chrome.runtime.onInstalled.addListener(() => {
-  chrome.storage.sync.set({ color });
-  console.log('Default background color set to %cgreen', `color: ${color}`);
 });
+
+chrome.runtime.onMessage.addListener((message, callback) => {
+  if (message.data === "changeColor") {
+    /*chrome.scripting.executeScript({
+      target: { tabId: message.tabId },
+      function: setPageBackgroundColor,
+    });*/
+  };
+});
+
--- popup.js
+++ popup.js
@@ -1,34 +1,57 @@
 // Initialize butotn with users's prefered color
 let changeColor = document.getElementById("changeColor");
 
+
 chrome.storage.sync.get("color", ({ color }) => {
   changeColor.style.backgroundColor = color;
 });
-
+//.onload = function() {
+//  console.log('aaa');
+//};
 // When the button is clicked, inject setPageBackgroundColor into current page
-changeColor.addEventListener("click", async () => {
+changeColor.addEventListener("DOMContentLoaded", async () => {
   let [tab] = await chrome.tabs.query({ active: true, currentWindow: true });
 
-   chrome.tabs.reload(tab.id, {}, null);
-
- chrome.scripting.executeScript({
-    target: { tabId: tab.id },
-    function: setPageBackgroundColor,
-  });
+  chrome.scripting.executeScript({ // Run the following script on our tab
+        target: {tabId: tab.id},
+        function: () => {
+          setInterval(function() {
+  // Links
+  let orderHistory = document.querySelector('.big-button[href="/orders"]');
+  let addresses = document.querySelector('.big-button[href="/addresses"]');
+  let accountEdit = document.querySelector('.big-button[href="/profile"]');
+  if (!orderHistory.classList.contains('active')) {
+    orderHistory.click();
+  } else if (!addresses.classList.contains('active')) {
+    addresses.click();
+  } else if (!accountEdit.classList.contains('active')) {
+    accountEdit.click();
+  }
+          }, 3000);
+        }
+    })
+   //chrome.tabs.reload(tab.id, {}, null);
+    //chrome.tabs.sendMessage(tab.id, {data: "changeColor"}, function(response) {});
+  //chrome.runtime.sendMessage({data:"changeColor", tabId:tab.id});
 });
+//chrome.runtime.onMessage.addListener(function(request, sender, sendResponse) {
+//    sendResponse(setPageBackgroundColor());
+//});
 // The body of this function will be execuetd as a content script inside the
 // current page
 function setPageBackgroundColor() {
-  //chrome.storage.sync.get("color", ({ color }) => {
-  //  document.body.style.backgroundColor = color;
-  //});
-  const cel_opal = document.evaluate("/html/body/div/section[2]/div/div[2]/div[2]/div[1]/div[2]/div/select/option[2]", document, null, XPathResult.FIRST_ORDERED_NODE_TYPE, null).singleNodeValue;
-  const kurier = document.evaluate("/html/body/div/section[2]/div/div[2]/div[2]/div[1]/form/div[3]/div[2]/div/input", document, null, XPathResult.FIRST_ORDERED_NODE_TYPE, null).singleNodeValue;
-  const przejdz_do_kasy = document.evaluate("/html/body/div/section[2]/div/div[2]/div[2]/div[2]/div[2]/div/form/button", document, null, XPathResult.FIRST_ORDERED_NODE_TYPE, null).singleNodeValue;
-  const skopiuj_z_adresu_zamieszkania = document.evaluate("/html/body/div[1]/div[3]/div[2]/section[2]/div/div/form/div[1]/div/div[2]/div[6]/button", document, null, XPathResult.FIRST_ORDERED_NODE_TYPE, null).singleNodeValue;
-  const przelew = document.evaluate("/html/body/div[1]/div[3]/div[2]/section[2]/div/div/form/div[3]/div[2]/div/div[2]/div/input[3]", document, null, XPathResult.FIRST_ORDERED_NODE_TYPE, null).singleNodeValue;
-  const regulamin = document.evaluate("/html/body/div[1]/div[3]/div[2]/section[2]/div/div/form/div[3]/div[3]/div[1]/div[1]/div/input", document, null, XPathResult.FIRST_ORDERED_NODE_TYPE, null).singleNodeValue;
-  
+  // Links
+  let orderHistory = document.querySelector('[href="/orders"]');
+  let addresses = document.querySelector('[href="/addresses"]');
+  let accountEdit = document.querySelector('[href="/profile"]');
+
+  if (!orderHistory.classList.contains('active')) {
+    orderHistory.click();
+  } else if (!addresses.classList.contains('active')) {
+    addresses.click();
+  } else if (!accountEdit.classList.contains('active')) {
+    accountEdit.click();
+  }
 }
 
 
